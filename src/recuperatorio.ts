5cfa98bb97073563977c45dfe74b4419a5eb3b1f
Elucietti <106998785+Elucietti@users.noreply.github.com>
2022-06-18 16:50:35 -0300

    recuperatorio crea funciones

--- src/recuperatorio.ts
+++ src/recuperatorio.ts
@@ -3,14 +3,23 @@ let ingresoCantidadProductos: number = Number(
 ); //longitud
 let producto: string[] = new Array(ingresoCantidadProductos);
 let compra: number[] = new Array(ingresoCantidadProductos);
-
 function detalle(arregloDetalle: string[]): void {
   for (let i: number = 0; i < arregloDetalle.length; i++) {
     producto = prompt(
       "Ingrese productos en el carrito " + ingresoCantidadProductos
     );
+    arregloDetalle[i] = producto;
   }
 }
 
-/*detalle(producto);
- */
+function ObtenerPrecios(arregloPrecios: number[]): void {
+  for (let i: number = 0; i < arregloPrecios.length; i++) {
+    let precios: number = Number(
+      prompt("ingrese precio unitario " + ingresoCantidadProductos)
+    );
+    arregloPrecios[i] = precios;
+  }
+}
+
+detalle(producto);
+ObtenerPrecios(compra);
